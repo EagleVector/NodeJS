f068b9030afc7687f87b6eb8d3714cc4e5eac863
EagleVector <kumarsubhambili3@gmail.com>
2023-06-23 18:10:13 +0530

    Modules in JS

--- hello.js
+++ hello.js
@@ -1,4 +1,25 @@
 // console.log(alert('hi'));
 // console.log(window);
 
-console.log('Hi There! JS');
+// console.log('Hi There! JS');
+
+// const math = require('./math');
+
+// console.log(math.add(2, 5));
+// console.log(math.subtract(2, 5));
+// console.log(math.multiply(2, 5));
+// console.log(math.divide(5, 2));
+// console.log(math);
+
+// const { add, subtract, multiply, divide } = require('./math');
+
+// console.log(add(2, 5));
+// console.log(subtract(2, 5));
+// console.log(multiply(2, 5));
+// console.log(divide(5, 2));
+// console.log(math);
+
+const math = require('./math');
+
+console.log(math.add(2, 5));
+console.log(math.sub(4, 1));
